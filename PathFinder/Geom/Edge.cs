88d26eaa83442045544e8d1d1b366f41d8b0eda1
Justica <atsuronreplaced@gmail.com>
2023-04-22 14:17:17 +0900

    First commit

---
+++ PathFinder/Geom/Edge.cs
@@ -0,0 +1,40 @@
+﻿namespace PathFinder.Geom
+{
+    public interface IEdge
+    {
+        public uint Start { get; init; }
+        public uint End { get; init; }
+        public uint Cost { get; init; }
+        public bool Directed { get; init; }
+    }
+
+    public abstract class BaseEdge : IEdge
+    {
+        public abstract uint Start { get; init; }
+        public abstract uint End { get; init; }
+        public abstract uint Cost { get; init; }
+        public abstract bool Directed { get; init; }
+
+        public override string ToString()
+        {
+            if (Cost == 1) return $"({Start} -> {End})";
+            return $"({Start} -> {End} : Cost={Cost})";
+        }
+    }
+
+    public class NonDirectionalEdge : BaseEdge
+    {
+        public override uint Start { get; init; }
+        public override uint End { get; init; }
+        public override uint Cost { get; init; }
+        public override bool Directed { get; init; }
+
+        public NonDirectionalEdge(uint start, uint end, uint cost=1)
+        {
+            Start = start;
+            End = end;
+            Cost = cost;
+            Directed = false;
+        }
+    }
+}
---
+++ PathFinder/Geom/Nodes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinder.Geom
+{
+    public class Nodes
+    {
+        public List<IEdge> Edges { get; init; }
+        public List<uint> NodeIndecies { get; init; }
+
+        public Nodes()
+        {
+            Edges = new List<IEdge>();
+            NodeIndecies = new List<uint>();
+        }
+
+        public Nodes(List<IEdge> edges)
+        {
+            Edges = edges;
+            var _ni = new HashSet<uint>(edges.Select(e => e.Start));
+            _ni.UnionWith(edges.Select(e => e.End));
+            NodeIndecies = _ni.ToList();
+            NodeIndecies.Sort();
+        }
+
+        public Nodes(List<IEdge> edges, List<uint> nodeIndecies)
+        {
+            Edges = edges;
+            NodeIndecies = nodeIndecies;
+            if (! CheckAllEdge())
+            {
+                throw new ArgumentException("未定義の Node が Edge の定義に含まれています。");
+            }
+        }
+
+        private bool CheckAllEdge()
+        {
+            var nodeSet = new HashSet<uint>(NodeIndecies);
+            return Edges.All(e => nodeSet.Contains(e.Start)) && Edges.All(e => nodeSet.Contains(e.End));
+        }
+
+        private bool CheckEdge(IEdge edge)
+        {
+            return NodeIndecies.Contains(edge.Start) && NodeIndecies.Contains(edge.End);
+        }
+    }
+}
