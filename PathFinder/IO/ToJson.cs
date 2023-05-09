4c7623e5c57e8eb064f462ff6d6b9fd29898158b
Justica <atsuronreplaced@gmail.com>
2023-05-09 18:03:50 +0900

    Add ToJson class

---
+++ PathFinder/IO/ToJson.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using PathFinder.Geom;
+
+namespace PathFinder.IO
+{
+    public static class ToJson
+    {
+        public static string Dumps(Graph graph, JsonSerializerOptions? options = default)
+        {
+            CheckGraph(graph);
+            if (options == default) options = CreateDefaultOptions();
+
+            return JsonSerializer.Serialize(graph, options);
+        }
+
+        public async static void Dump(string fileName, Graph graph, JsonSerializerOptions? options = default)
+        {
+            CheckGraph(graph);
+            if (options == default) options = CreateDefaultOptions();
+
+            using FileStream createStream = File.Create(fileName);
+            await JsonSerializer.SerializeAsync(createStream, graph, options);
+            await createStream.DisposeAsync();
+        }
+
+        private static JsonSerializerOptions CreateDefaultOptions() => new() { WriteIndented = true };
+
+        private static void CheckGraph (in Graph graph)
+        {
+            if (graph == null) throw new ArgumentException("Graph が null です。");
+            if (string.IsNullOrEmpty(graph.NodeType)) throw new ArgumentException("NodeType が未定義です。");
+            if (string.IsNullOrEmpty(graph.EdgeType)) throw new ArgumentException("EdgeType が未定義です。");
+        } 
+    }
+}
---
+++ PathFinderTests/IO/ToJsonTests.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text.Json;
+using PathFinder.Geom;
+using PathFinder.IO;
+
+namespace PathFinderTests.IO
+{
+    public class ToJsonTests
+    {
+        private Graph GenSampleGraph()
+        {
+            var edges = new List<IEdge>()
+            {
+                new NonDirectionalEdge(0, 1),
+                new NonDirectionalEdge(0, 2),
+                new NonDirectionalEdge(1, 2)
+            };
+            var nodes = new List<INode>()
+            {
+                new CoreNode(0),
+                new CoreNode(1),
+                new CoreNode(2)
+            };
+            return new Graph(edges, nodes);
+        }
+
+        [Fact]
+        public void DumpsTest()
+        {
+            var graph = GenSampleGraph();
+            var actual = ToJson.Dumps(graph, new JsonSerializerOptions() { WriteIndented = false});
+            var expected = "{\"EdgeType\":\"NonDirectionalEdge\",\"NodeType\":\"CoreNode\",\"Edges\":[{\"Start\":0,\"End\":1,\"Cost\":1,\"Directed\":false},{\"Start\":0,\"End\":2,\"Cost\":1,\"Directed\":false},{\"Start\":1,\"End\":2,\"Cost\":1,\"Directed\":false}],\"Nodes\":[{\"Index\":0},{\"Index\":1},{\"Index\":2}]}";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DumpsUninitilizedGraphTest()
+        {
+            var emptyGraph = new Graph();
+            Assert.Throws<ArgumentException>(() => ToJson.Dumps(emptyGraph));
+        }
+
+        [Fact]
+        public void DumpTest()
+        {
+            // Generate sample graph
+            var graph = GenSampleGraph();
+
+            // Save to json
+            var path = "output.json";
+            ToJson.Dump(path, graph);
+
+            // Test
+            var isExists = File.Exists(path);
+            Assert.True(isExists);
+
+            // Remove output file
+            File.Delete(path);
+        }
+    }
+}
